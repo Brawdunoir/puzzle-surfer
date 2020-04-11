697798bb9e5bfead69f7d73ded23e6df6b9632b7
Brawdunoir <yannlacroix32@gmail.com>
2020-04-11 20:59:45 +0200

    update/display grid with resize in real time
    test avec un nouveau component grid-unit qui représente un seul bloc de la grille et il y en aurait i*j mais rien de très probant...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
+import { GridUnitComponent } from './grid-unit/grid-unit.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MatButtonModule } from '@angular/material/button';
     GridComponent,
     ScoreComponent,
     GameComponent,
-    HomeComponent
+    HomeComponent,
+    GridUnitComponent
   ],
   imports: [
     BrowserModule,
--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-grid',
@@ -6,22 +7,34 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./grid.component.scss']
 })
 export class GridComponent implements OnInit {
-  @Input() nbRows:number;
-  @Input() nbColumns:number;
+  @Input() nbRows: number;
+  @Input() nbColumns: number;
+  blocUnit: number;
   grid: boolean[][];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.initGrid();
+    this.getUnit();
+    this.grid[2][2] = true;
+  }
+  
+  // Instancier la grille logique
+  initGrid() {
     this.grid = []
     for (let i = 0; i < this.nbRows; i++) {
       this.grid[i] = [];
       for (let j = 0; j < this.nbColumns; j++) {
-        this.grid[i][j]= false;
-      }    
+        this.grid[i][j] = false;
+      }
     }
   }
-
   
-
+  // Récupérer l'unité d'un bloc pour le background-size
+  @HostListener('window:resize', ['$event'])
+  getUnit(event?) {
+    this.blocUnit = window.innerWidth / this.nbColumns;
+  }
 }
