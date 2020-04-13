afceb561371e1c157bda5cfafec18b4a366d096e
Brawdunoir <yannlacroix32@gmail.com>
2020-04-13 16:06:34 +0200

    update/app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms'
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
 import { ScoreComponent } from './score/score.component';
 import { GameComponent } from './game/game.component';
 import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SingleBlocComponent } from './single-bloc/single-bloc.component';
 
-import { MatButtonModule } from '@angular/material/button';
-import { GridUnitComponent } from './grid-unit/grid-unit.component';
 
 @NgModule({
   declarations: [
@@ -20,12 +23,14 @@ import { GridUnitComponent } from './grid-unit/grid-unit.component';
     ScoreComponent,
     GameComponent,
     HomeComponent,
-    GridUnitComponent
+    SingleBlocComponent,
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     MatButtonModule,
+    DragDropModule,
+    MatGridListModule,
      RouterModule.forRoot([
        { path: '', component: HomeComponent },
        { path: 'game', component: GameComponent },
