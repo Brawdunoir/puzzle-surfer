34cf908a605e9a42e9bf8f443389447f1fbaa2d2
Brawdunoir <yannlacroix32@gmail.com>
2020-04-13 16:03:47 +0200

    update/game-component
    game devient le cerveau

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { EventEmitter } from 'protractor';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  blocUnit: number;
+  dimensions: number = 4;
+  index: number;
+  eventIndexReceived: Subject<void> = new Subject<void>();
+  private grid: boolean[] = [];
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.getInitUnit();
+    this.initGrid();
+  }
+
+  initGrid() {
+    for (let i = 0; i < this.dimensions * this.dimensions; i++) {
+      this.grid[i] = false;
+      console.log(i);
+    }
+  }
+  // Récupérer l'unité d'un bloc pour le background-size
+  @HostListener('window:resize', ['$event'])
+  getDynamicUnit(event?) {
+    this.blocUnit = window.innerWidth / this.dimensions;
+  }
+  
+  getInitUnit() {
+    this.blocUnit = window.innerWidth / this.dimensions;
+  }
+
+  getIndex(index: number) {
+    this.index = index;
+    this.grid[index] = true;
+    this.eventIndexReceived.next();
   }
 
 }
