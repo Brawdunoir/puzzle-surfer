ed70c02f0bb5b860b0672ff1463bbe0998f95e76
Brawdunoir <yannlacroix32@gmail.com>
2020-04-13 16:32:32 +0200

    update/game-component scores

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -11,7 +11,8 @@ export class GameComponent implements OnInit {
 
   blocUnit: number;
   dimensions: number = 4;
-  index: number;
+  // index: number;
+  private score: number = 0;
   eventIndexReceived: Subject<void> = new Subject<void>();
   private grid: boolean[] = [];
 
@@ -40,9 +41,47 @@ export class GameComponent implements OnInit {
   }
 
   getIndex(index: number) {
-    this.index = index;
+    // this.index = index;
     this.grid[index] = true;
+
+    // On regarde si on a réussit à compléter une ligne/colonne
+    this.checkGrid();
+
+    // On update le visuel dans grid-component
     this.eventIndexReceived.next();
+  }
+
+  checkGrid() {
+    let col: boolean;
+    let lig: boolean;
+    // On vérifie si c'est complet
+    for (let i = 0; i < this.dimensions; i++) {
+      lig = true;
+      col = true;
+      for (let j = 0; j < this.dimensions; j++) {
+        // Lignes
+        if (!this.grid[i * this.dimensions + j]) {
+          lig = false;
+        }
+        // Colonnes
+        if (!this.grid[i + j * this.dimensions]) {
+          col = false;
+        }
+      }
+      // On retire si c'est toujours vrai
+      if (lig) {
+        for (let j = 0; j < this.dimensions; j++) {
+          this.grid[i * this.dimensions + j] = false;
+        }
+        this.score += this.dimensions;
+      }
+      if (col) {
+        for (let j = 0; j < this.dimensions; j++) {
+          this.grid[i + j * this.dimensions] = false;
+        }
+        this.score += this.dimensions;
+      }
+    }
   }
 
 }
