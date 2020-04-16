3977bae16a898a397fba500cd92fd72e0817b41c
Brawdunoir <yannlacroix32@gmail.com>
2020-04-16 20:37:24 +0200

    update/delete OnInit import

--- src/app/multi.service.ts
+++ src/app/multi.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BasicService } from './basic.service';
 import { PieceService } from './piece.service';
 
