d5f1173aca327ac23699726409127251359ed48b
Brawdunoir <yannlacroix32@gmail.com>
2020-04-16 04:40:38 +0200

    update/add new pieces
    43 pieces !!

--- src/app/piece.service.ts
+++ src/app/piece.service.ts
@@ -12,7 +12,7 @@ export class PieceService {
 
   formes = [
     {
-      // * Tetris
+      // * Tetris - 1
       dimensions: {
         x: 3,
         y: 2
@@ -24,7 +24,52 @@ export class PieceService {
         { x: 2, y: 1 },
       ],
       jumps: [1, this.dim, this.dim + 1, this.dim + 2],
-      color: '#d50000',
+      color: '#fff59d',
+    },
+    {
+      // * Tetris - 2
+      dimensions: {
+        x: 3,
+        y: 2
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 0 },
+      ],
+      jumps: [0, 1, 2, this.dim + 1],
+      color: '#fff59d',
+    },
+    {
+      // * Tetris - 3
+      dimensions: {
+        x: 2,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 1, y: 1 },
+      ],
+      jumps: [0, this.dim, this.dim + 1, 2 * this.dim],
+      color: '#fff59d',
+    },
+    {
+      // * Tetris - 4
+      dimensions: {
+        x: 2,
+        y: 3
+      },
+      positions: [
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 1, y: 2 },
+        { x: 0, y: 1 },
+      ],
+      jumps: [1, this.dim, this.dim + 1, 2 * this.dim],
+      color: '#fff59d',
     },
     {
       // * Medium Cube
@@ -62,6 +107,38 @@ export class PieceService {
       color: '#aa00ff',
     },
     {
+      // * Medium Cross
+      dimensions: {
+        x: 3,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 1 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+        { x: 1, y: 2 },
+      ],
+      jumps: [1, this.dim, this.dim + 1, this.dim + 2, 2 * this.dim + 1],
+      color: '#d50000',
+    },
+    {
+      // * Medium Cross
+      dimensions: {
+        x: 3,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 1 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+        { x: 1, y: 2 },
+      ],
+      jumps: [1, this.dim, this.dim + 1, this.dim + 2, 2 * this.dim + 1],
+      color: '#d50000',
+    },
+    {
       // * Small Cube
       dimensions: {
         x: 1,
@@ -74,7 +151,7 @@ export class PieceService {
       color: '#304ffe',
     },
     {
-      // * 2-Horizontal Line
+      // * 2 Horizontal Line
       dimensions: {
         x: 2,
         y: 1
@@ -87,7 +164,7 @@ export class PieceService {
       color: '#9575cd',
     },
     {
-      // * 3-Horizontal Line
+      // * 3 Horizontal Line
       dimensions: {
         x: 3,
         y: 1
@@ -101,7 +178,24 @@ export class PieceService {
       color: '#ffd54f',
     },
     {
-      // * 4-Horizontal Line
+      // * 3 Horizontal Line Double
+      dimensions: {
+        x: 3,
+        y: 2
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 0, y: 1 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+      ],
+      jumps: [0, 1, 2, this.dim, this.dim + 1, this.dim + 2],
+      color: '#ffd54f',
+    },
+    {
+      // * 4 Horizontal Line
       dimensions: {
         x: 4,
         y: 1
@@ -116,7 +210,26 @@ export class PieceService {
       color: '#00c853',
     },
     {
-      // * 5-Horizontal Line
+      // * 4 Horizontal Line Double
+      dimensions: {
+        x: 4,
+        y: 2
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 3, y: 0 },
+        { x: 0, y: 1 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+        { x: 3, y: 1 },
+      ],
+      jumps: [0, 1, 2, 3, this.dim, this.dim + 1, this.dim + 2, this.dim + 3],
+      color: '#00c853',
+    },
+    {
+      // * 5 Horizontal Line
       dimensions: {
         x: 5,
         y: 1
@@ -132,7 +245,143 @@ export class PieceService {
       color: '#dd2c00',
     },
     {
-      // * Crooked-01
+      // * 5 Horizontal Line Double
+      dimensions: {
+        x: 5,
+        y: 2
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 3, y: 0 },
+        { x: 4, y: 0 },
+        { x: 0, y: 1 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+        { x: 3, y: 1 },
+        { x: 4, y: 1 },
+      ],
+      jumps: [0, 1, 2, 3, 4, this.dim, this.dim + 1, this.dim + 2, this.dim + 3, this.dim + 4],
+      color: '#dd2c00',
+    },
+    {
+      // * 2 Vertical Line
+      dimensions: {
+        x: 1,
+        y: 2
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+      ],
+      jumps: [0, this.dim],
+      color: '#9575cd',
+    },
+    {
+      // * 3 Vertical Line
+      dimensions: {
+        x: 1,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+      ],
+      jumps: [0, this.dim, 2 * this.dim],
+      color: '#ffd54f',
+    },
+    {
+      // * 3 Vertical Line Double
+      dimensions: {
+        x: 2,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 1, y: 2 },
+      ],
+      jumps: [0, 1, this.dim, this.dim + 1, 2 * this.dim, 2 * this.dim + 1],
+      color: '#ffd54f',
+    },
+    {
+      // * 4 Vertical Line
+      dimensions: {
+        x: 1,
+        y: 4
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 0, y: 3 },
+      ],
+      jumps: [0, this.dim, 2 * this.dim, 3 * this.dim],
+      color: '#00c853',
+    },
+    {
+      // * 4 Vertical Line Double
+      dimensions: {
+        x: 2,
+        y: 4
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 0, y: 3 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 1, y: 2 },
+        { x: 1, y: 3 },
+      ],
+      jumps: [0, 1, this.dim, this.dim + 1, 2 * this.dim, 2 * this.dim + 1, 3 * this.dim, 3 * this.dim + 1],
+      color: '#00c853',
+    },
+    {
+      // * 5 Vertical Line
+      dimensions: {
+        x: 1,
+        y: 5
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 0, y: 3 },
+        { x: 0, y: 4 },
+      ],
+      jumps: [0, this.dim, 2 * this.dim, 3 * this.dim, 4 * this.dim],
+      color: '#dd2c00',
+    },
+    {
+      // * 5 Vertical Line Double
+      dimensions: {
+        x: 2,
+        y: 5
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 0, y: 3 },
+        { x: 0, y: 4 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 1, y: 2 },
+        { x: 1, y: 3 },
+        { x: 1, y: 4 },
+      ],
+      jumps: [0, 1, this.dim, this.dim + 1, 2 * this.dim, 2 * this.dim + 1, 3 * this.dim, 3 * this.dim + 1, 4 * this.dim, 4 * this.dim + 1],
+      color: '#dd2c00',
+    },
+    {
+      // * Medium Crooked - 1
       dimensions: {
         x: 2,
         y: 2
@@ -146,21 +395,21 @@ export class PieceService {
       color: '#ffab00',
     },
     {
-      // * Crooked-02
+      // * Medium Crooked - 2
       dimensions: {
         x: 2,
         y: 2
       },
       positions: [
         { x: 0, y: 0 },
-        { x: 0, y: 1 },
+        { x: 1, y: 0 },
         { x: 0, y: 1 },
       ],
       jumps: [0, 1, this.dim],
       color: '#81d4fa',
     },
     {
-      // * Crooked-03
+      // * Medium Crooked - 3
       dimensions: {
         x: 2,
         y: 2
@@ -174,7 +423,7 @@ export class PieceService {
       color: '#ffe082',
     },
     {
-      // * Crooked-04
+      // * Medium Crooked - 4
       dimensions: {
         x: 2,
         y: 2
@@ -188,7 +437,7 @@ export class PieceService {
       color: '#00c853',
     },
     {
-      // * Medium Horizontal L - 01
+      // * Medium Horizontal L - 1
       dimensions: {
         x: 4,
         y: 2
@@ -204,7 +453,7 @@ export class PieceService {
       color: '#6200ea',
     },
     {
-      // * Medium Horizontal L - 02
+      // * Medium Horizontal L - 2
       dimensions: {
         x: 4,
         y: 2
@@ -217,10 +466,10 @@ export class PieceService {
         { x: 0, y: 1 },
       ],
       jumps: [0, 1, 2, 3, this.dim],
-      color: '#6200ea',
+      color: '#2962ff',
     },
     {
-      // * Medium Horizontal L - 03
+      // * Medium Horizontal L - 3
       dimensions: {
         x: 4,
         y: 2
@@ -233,23 +482,211 @@ export class PieceService {
         { x: 0, y: 0 },
       ],
       jumps: [0, this.dim, this.dim + 1, this.dim + 2, this.dim + 3],
-      color: '#6200ea',
+      color: '#80deea',
     },
     {
-      // * Medium Horizontal L - 02
+      // * Medium Horizontal L - 4
       dimensions: {
         x: 4,
+        y: 2
+      },
+      positions: [
+        { x: 0, y: 1 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+        { x: 3, y: 1 },
+        { x: 3, y: 0 },
+      ],
+      jumps: [this.dim, this.dim + 1, this.dim + 2, this.dim + 3, 3],
+      color: '#ffab91',
+    },
+    {
+      // * Large Crooked - 1
+      dimensions: {
+        x: 3,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 2, y: 1 },
+        { x: 2, y: 2 },
+      ],
+      jumps: [0, 1, 2, this.dim + 2, 2 * this.dim + 2],
+      color: '#d50000',
+    },
+    {
+      // * Large Crooked - 2
+      dimensions: {
+        x: 3,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 2, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+      ],
+      jumps: [0, 1, 2, this.dim, 2 * this.dim],
+      color: '#d50000',
+    },
+    {
+      // * Large Crooked - 3
+      dimensions: {
+        x: 3,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 1, y: 2 },
+        { x: 2, y: 2 },
+      ],
+      jumps: [0, this.dim, 2 * this.dim, 2 * this.dim + 1, 2 * this.dim + 2],
+      color: '#d50000',
+    },
+    {
+      // * Large Crooked - 4
+      dimensions: {
+        x: 3,
+        y: 3
+      },
+      positions: [
+        { x: 2, y: 0 },
+        { x: 2, y: 1 },
+        { x: 2, y: 2 },
+        { x: 0, y: 2 },
+        { x: 1, y: 2 },
+      ],
+      jumps: [2, this.dim + 2, 2 * this.dim + 2, 2 * this.dim, 2 * this.dim + 1],
+      color: '#d50000',
+    },
+    {
+      // * Medium Vertical L - 1
+      dimensions: {
+        x: 2,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 1, y: 2 },
+      ],
+      jumps: [0, 1, this.dim + 1, 2 * this.dim + 1],
+      color: '#d50000',
+    },
+    {
+      // * Medium Vertical L - 2
+      dimensions: {
+        x: 2,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+      ],
+      jumps: [0, 1, this. dim, 2 * this.dim],
+      color: '#d50000',
+    },
+    {
+      // * Medium Vertical L - 3
+      dimensions: {
+        x: 2,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 1, y: 2 },
+      ],
+      jumps: [0, this.dim, 2 * this. dim, 2 * this.dim + 1],
+      color: '#d50000',
+    },
+    {
+      // * Medium Vertical L - 4
+      dimensions: {
+        x: 2,
+        y: 3
+      },
+      positions: [
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 1, y: 2 },
+        { x: 0, y: 2 },
+      ],
+      jumps: [1, this.dim + 1, 2 * this. dim + 1, 2 * this.dim],
+      color: '#d50000',
+    },
+    {
+      // * U - 1
+      dimensions: {
+        x: 3,
+        y: 2
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 0, y: 1 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+        { x: 2, y: 0 },
+      ],
+      jumps: [0, 2, this.dim, this.dim + 1, this. dim + 2],
+      color: '#d50000',
+    },
+    {
+      // * U - 2
+      dimensions: {
+        x: 3,
         y: 2
       },
       positions: [
         { x: 0, y: 0 },
         { x: 1, y: 0 },
         { x: 2, y: 0 },
-        { x: 3, y: 0 },
         { x: 0, y: 1 },
+        { x: 2, y: 1 },
       ],
-      jumps: [0, 1, 2, 3, this.dim],
-      color: '#6200ea',
+      jumps: [0, 1, 2, this.dim, this.dim + 2],
+      color: '#d50000',
+    },
+    {
+      // * U - 3
+      dimensions: {
+        x: 2,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 0, y: 1 },
+        { x: 0, y: 2 },
+        { x: 1, y: 2 },
+      ],
+      jumps: [0, 1, this.dim, 2 * this.dim, 2 * this.dim + 1],
+      color: '#d50000',
+    },
+    {
+      // * U - 4
+      dimensions: {
+        x: 2,
+        y: 3
+      },
+      positions: [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 1, y: 2 },
+        { x: 0, y: 2 },
+      ],
+      jumps: [0, 1, this.dim + 1, 2 * this.dim + 1, 2 * this.dim],
+      color: '#d50000',
     },
   ];
 
