6d1f689ded18f837f71ab2f1e8916ecf29993d17
Romain <romain.gianolio@outlook.com>
2020-04-18 01:37:16 +0200

    update/deleted pieces

--- src/app/piece.service.ts
+++ src/app/piece.service.ts
@@ -12,12 +12,10 @@ export class PieceService {
   color = {
     tetris: '#ba68c8',
     cube: '#fff176',
-    cross: '#e57373',
     line: '#4fc3f7',
     doubleLine: '#81c784',
     crooked: '#ff8a65',
-    l: '#7986cb',
-    u: '#a1887f',
+    l: '#e57373',
   };
 
   formes = [
@@ -136,43 +134,6 @@ export class PieceService {
       ],
       color: this.color.cube,
     },
-    {
-      // * Medium Cross
-      dimensions: {
-        x: 3,
-        y: 3,
-      },
-      positions: [
-        { x: 0, y: 1 },
-        { x: 1, y: 0 },
-        { x: 1, y: 1 },
-        { x: 2, y: 1 },
-        { x: 1, y: 2 },
-      ],
-      jumps: [1, this.dim, this.dim + 1, this.dim + 2, 2 * this.dim + 1],
-      color: this.color.cross,
-    },
-    // {
-    //   // * Large Cross
-    //   dimensions: {
-    //     x: 5,
-    //     y: 5
-    //   },
-    //   positions: [
-    //     { x: 2, y: 0 },
-    //     { x: 2, y: 1 },
-    //     { x: 0, y: 2 },
-    //     { x: 1, y: 2 },
-    //     { x: 2, y: 2 },
-    //     { x: 3, y: 2 },
-    //     { x: 4, y: 2 },
-    //     { x: 2, y: 3 },
-    //     { x: 2, y: 4 },
-    //   ],
-    //   jumps: [2, this.dim + 2, 2 * this.dim, 2 * this.dim + 1, 2 * this.dim + 2, 2 * this.dim + 3,
-    //     2 * this.dim + 4, 3 * this.dim + 2, 4 * this.dim + 2],
-    //   color: this.color.cross,
-    // },
     {
       // * 2 Horizontal Line
       dimensions: {
@@ -683,70 +644,6 @@ export class PieceService {
       ],
       jumps: [1, this.dim + 1, 2 * this.dim + 1, 2 * this.dim],
       color: this.color.l,
-    },
-    {
-      // * U - 1
-      dimensions: {
-        x: 3,
-        y: 2,
-      },
-      positions: [
-        { x: 0, y: 0 },
-        { x: 0, y: 1 },
-        { x: 1, y: 1 },
-        { x: 2, y: 1 },
-        { x: 2, y: 0 },
-      ],
-      jumps: [0, 2, this.dim, this.dim + 1, this.dim + 2],
-      color: this.color.u,
-    },
-    {
-      // * U - 2
-      dimensions: {
-        x: 3,
-        y: 2,
-      },
-      positions: [
-        { x: 0, y: 0 },
-        { x: 1, y: 0 },
-        { x: 2, y: 0 },
-        { x: 0, y: 1 },
-        { x: 2, y: 1 },
-      ],
-      jumps: [0, 1, 2, this.dim, this.dim + 2],
-      color: this.color.u,
-    },
-    {
-      // * U - 3
-      dimensions: {
-        x: 2,
-        y: 3,
-      },
-      positions: [
-        { x: 0, y: 0 },
-        { x: 1, y: 0 },
-        { x: 0, y: 1 },
-        { x: 0, y: 2 },
-        { x: 1, y: 2 },
-      ],
-      jumps: [0, 1, this.dim, 2 * this.dim, 2 * this.dim + 1],
-      color: this.color.u,
-    },
-    {
-      // * U - 4
-      dimensions: {
-        x: 2,
-        y: 3,
-      },
-      positions: [
-        { x: 0, y: 0 },
-        { x: 1, y: 0 },
-        { x: 1, y: 1 },
-        { x: 1, y: 2 },
-        { x: 0, y: 2 },
-      ],
-      jumps: [0, 1, this.dim + 1, 2 * this.dim + 1, 2 * this.dim],
-      color: this.color.u,
     },
   ];
 
