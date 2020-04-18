10cfb7531d92642c74bfbecd8d12c98b00f2cf99
Romain <romain.gianolio@outlook.com>
2020-04-18 11:57:59 +0200

    update/translate pieces
    Co-authored-by: Brawdunoir <yannlacroix32b@gmail.com>

--- src/app/variable.service.ts
+++ src/app/variable.service.ts
@@ -13,9 +13,10 @@ export class VariableService {
   tileShadow =
     '0 6.4px 14.4px 0 rgba(0,0,0,.132), 0 1.2px 3.6px 0 rgba(0,0,0,.108)';
 
+  pieceTranslate = 80;
   pieceTransition = 'transform 300ms cubic-bezier(.21,.74,.04,1.02)';
-  pieceHalf = 'scale(0.5)';
-  pieceFull = 'scale(1)';
+  pieceHalf = 'scale(0.5) translateY(0)';
+  pieceFull = 'scale(1) translateY(-' + this.pieceTranslate + 'px)';
 
   constructor() {}
 
