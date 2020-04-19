d3933baec06047202d100d12508c5e8015d79444
Brawdunoir <yannlacroix32@gmail.com>
2020-04-19 23:30:00 +0200

    update/style of settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { VariableService } from '../variable.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
-
   min = 8;
   max = 30;
   value = 10;
+  color = 'primary';
 
-  constructor() { }
+  constructor(private variables: VariableService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
