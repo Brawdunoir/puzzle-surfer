e8c514bb78a4b6fbb7ae39d449e3d0ae4777ba54
Romain <romain.gianolio@outlook.com>
2020-04-19 18:30:00 +0200

    update/lowered by 2 the shadow opacity

--- src/app/variable.service.ts
+++ src/app/variable.service.ts
@@ -11,7 +11,7 @@ export class VariableService {
   tilePutDelay = 100; // millisecondes
   tileDeleteDelay = 220;
   tileShadow =
-    '0 6.4px 14.4px 0 rgba(0,0,0,.132), 0 1.2px 3.6px 0 rgba(0,0,0,.108)';
+    '0 6.4px 14.4px 0 rgba(0,0,0,.066), 0 1.2px 3.6px 0 rgba(0,0,0,.05)';
 
   pieceTranslate = 100;
   pieceTransition = 'transform 300ms cubic-bezier(.21,.74,.04,1.02)';
