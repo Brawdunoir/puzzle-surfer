9373d44c3ffcfb8349dac0bce91201729894d0eb
Romain <romain.gianolio@outlook.com>
2020-04-19 14:20:38 +0200

    feature/storage service

---
+++ src/app/storage.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService {
+  private storage: Storage = window.localStorage;
+
+  constructor() {}
+
+  get(key: string): string {
+    return this.storage.getItem(key);
+  }
+
+  store(key: string, value: string): void {
+    this.storage.setItem(key, value);
+  }
+
+  delete(key: string): void {
+    this.storage.deleteItem(key);
+  }
+
+  deleteAll(): void {
+    this.storage.clear();
+  }
+}
