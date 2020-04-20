fbf5bc9b43fb7f35e63f0b48e01dcaf02ccfdf31
Brawdunoir <yannlacroix32@gmail.com>
2020-04-20 21:37:20 +0200

    update/almost finish, lack localStorage

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -7,12 +7,40 @@ import { VariableService } from '../variable.service';
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  // Slider
   min = 8;
   max = 30;
   value = 10;
   color = 'primary';
+  // Themes
+  nbStyle = 4;
+  nbColor = 3;
+  currentStyle = 'amoled';
+  currentColor = 'sepia';
 
   constructor(private variables: VariableService) {}
 
   ngOnInit(): void {}
+
+  changeGridDimension(event: any) {
+    // TODO changer avec service localStorage.
+  }
+
+  setFocus(theme: string, value: string) {
+    const style = {
+      'background-color': value === this.currentStyle ? 'black' : 'white',
+    };
+    const color = {
+      'background-color': value === this.currentColor ? 'black' : 'white',
+    };
+    return theme === 'color' ? color : style;
+  }
+
+  changeTheme(theme: string, value: string): void {
+    if (theme === 'color') {
+      this.currentColor = value;
+    } else {
+      this.currentStyle = value;
+    }
+  }
 }
