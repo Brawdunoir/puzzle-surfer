2e53acfdad2edec7be38f3616f97aa753f09e5f1
Yann <yannlacroix32b@gmail.com>
2020-04-22 22:06:28 +0200

    add/settings service to feature/game

---
+++ src/app/settings.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { StorageService } from './storage.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SettingsService {
+  body = document.querySelector('body').classList;
+
+  constructor(private storage: StorageService) {}
+
+  getTheme(): string {
+    const currentTheme = this.storage.get('theme');
+    return currentTheme ? currentTheme : 'light';
+  }
+
+  setTheme(theme: string): void {
+    this.clearTheme();
+    this.body.add(theme);
+    this.storage.store('theme', theme);
+  }
+
+  clearTheme(): void {
+    this.body.remove('light');
+    this.body.remove('dark');
+    this.body.remove('amoled');
+  }
+}
