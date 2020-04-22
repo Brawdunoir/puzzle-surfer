86da91188546ab08cd9456800f3c7cdaf94ab34c
Yann <yannlacroix32b@gmail.com>
2020-04-22 11:50:24 +0200

    feature/best score now in storage

--- src/app/score.service.ts
+++ src/app/score.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ScoreService {
   update: Subject<number> = new Subject();
+
+  constructor(private storage: StorageService) {}
 
   add(score: number) {
     this.update.next(score);
@@ -19,5 +22,21 @@ export class ScoreService {
     return Math.round(0.4 * Math.pow(combo, 2) + 1 * combo) * dim;
   }
 
-  constructor() {}
+  updateBest(newBest: number): void {
+    // Verify this new score is better than previous
+    if (
+      isNaN(+this.storage.get('bestScore')) ||
+      +this.storage.get('bestScore') < newBest
+    ) {
+      // Store it
+      this.storage.store('bestScore', newBest.toString());
+    }
+  }
+
+  getBest(): number {
+    if (isNaN(+this.storage.get('bestScore'))) {
+      return 0;
+    }
+    return +this.storage.get('bestScore');
+  }
 }
--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -8,7 +8,7 @@ import { GameService } from '../game.service';
   styleUrls: ['./score.component.scss'],
 })
 export class ScoreComponent implements OnInit, OnDestroy {
-  bestScore = 0; // TODO Changer pour enregistrer le score dans de la data
+  bestScore = this.scoreService.getBest();
   currentScore = 0;
 
   update: any;
@@ -25,7 +25,9 @@ export class ScoreComponent implements OnInit, OnDestroy {
     });
 
     this.myRestart = this.gameService.restart.subscribe(() => {
-      this.restart();
+      this.scoreService.updateBest(this.currentScore);
+      this.bestScore = this.scoreService.getBest();
+      this.currentScore = 0;
     });
   }
 
@@ -34,10 +36,10 @@ export class ScoreComponent implements OnInit, OnDestroy {
     this.myRestart.unsubscribe();
   }
 
-  restart(): void {
-    if (this.currentScore > this.bestScore) {
-      this.bestScore = this.currentScore;
-    }
-    this.currentScore = 0;
-  }
+  // restart(): void {
+  //   if (this.currentScore > this.bestScore) {
+  //     this.bestScore = this.currentScore;
+  //   }
+  //   this.currentScore = 0;
+  // }
 }
